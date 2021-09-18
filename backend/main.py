478d5898d3f3ea650e933a4b8384f53aac20d71e
Zeyu-Li <zeyu.li23@gmail.com>
2021-09-18 15:57:24 -0600

    updated backend to include dumby data

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 # for windows phionthrium\Scripts\activate
-
-from flask import Flask, request
+import json
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
@@ -9,6 +9,20 @@ def main():
     # user is window.ethereum public key
     user = request.args.get('user')
     return {"message": f"hello {user}"}
+
+@app.route("/api/browse/")
+def browse():
+    # dumby info
+    content = [{
+        "name": "LLVM",
+        "description": "The LLVM Project is a collection of modular and reusable compiler and toolchain technologies. Despite its name, LLVM has little to do with traditional virtual machines. The name \"LLVM\" itself is not an acronym; it is the full name of the project.",
+        "img": None,
+    }, {
+        "name": "phionthrium",
+        "description": "this.self",
+        "img": None,
+    }, ]
+    return Response(json.dumps(content),  mimetype='application/json')
 
 @app.errorhandler(500)
 def server_error(e):

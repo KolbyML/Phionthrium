18dfbc4d7a47f9d680254a5e9a6cdc0684db05e3
Zeyu-Li <zeyu.li23@gmail.com>
2021-09-18 10:47:22 -0600

    added 500 error handler

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,11 @@ def main():
     user = request.args.get('user')
     return {"message": f"hello {user}"}
 
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return {"error": "500"}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
